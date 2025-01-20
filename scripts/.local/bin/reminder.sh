9440f9359a0fb11b07d3093e204f37901d67f24b
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-01-20 17:15:31 +0330

    Fix typo

--- scripts/.local/bin/reminder.sh
+++ scripts/.local/bin/reminder.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 reminder_file="$HOME/Documents/reminders.txt"
-reminder_text="$(rofi -dmenu -p "what's is the reminder?")"
-printf "$reminder_text\n\n" >> "$reminder_file"
+reminder_text="$(rofi -dmenu -p "what is the reminder?")"
+printf "$reminder_text\n\n" >>"$reminder_file"
