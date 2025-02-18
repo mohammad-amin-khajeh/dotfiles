24e1dae39662f260e07c8305f0ad5a4da2980801
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-02-18 23:09:02 +0330

    load tmuxp leetcode session instead of launching sessionizer in launch_tmux.sh

--- scripts/.local/bin/launch_tmux.sh
+++ scripts/.local/bin/launch_tmux.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-path_to_tmux_sessionizer=".local/bin/tmux-sessionizer"
+# path_to_tmux_sessionizer=".local/bin/tmux-sessionizer"
+tmuxp_layout="leetcode"
 if pgrep tmux &>/dev/null; then
   tmux attach
 else
-  "$path_to_tmux_sessionizer"
+  tmuxp load "$tmuxp_layout"
 fi
