9c6338a9c249f431f9dd7d52e9bb2b64eea38d37
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-03-04 23:34:07 +0330

    Use the proper printf syntax in reminder.sh

--- scripts/.local/bin/reminder.sh
+++ scripts/.local/bin/reminder.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 reminder_file="$HOME/Documents/reminders.txt"
 reminder_text="$(rofi -dmenu -p "what is the reminder?")"
-printf "$reminder_text\n\n" >>"$reminder_file"
+printf "%s\n\n" "$reminder_text" >>"$reminder_file"
