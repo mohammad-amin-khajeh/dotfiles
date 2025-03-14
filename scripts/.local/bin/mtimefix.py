1bd23e090a24dec1203da28281a0d8110ac23ed7
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-03-14 13:54:05 +0330

    make mtimefix.py more resistant
    now works on arabic and persian numerals as well
    and also includes prefixes like 'image-' and 'vid-' among other things
    and lastly, the filename can have one or more dashes between dates

--- scripts/.local/bin/mtimefix.py
+++ scripts/.local/bin/mtimefix.py
@@ -5,10 +5,22 @@ from glob import glob
 from subprocess import call
 
 files = set(glob("*.jpg") + glob("*.png") + glob("*.mp4"))
+intab = "۱۲۳۴۵۶۷۸۹۰١٢٣٤٥٦٧٨٩٠"
+outtab = "12345678901234567890"
+translation_table = str.maketrans(intab, outtab)
 
 for file in files:
-    if dates := re.search(r"(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})", file):
+    if dates := re.search(
+        r".*(\d{4})-*(\d{2})-*(\d{2})_(\d{2})-*(\d{2})-*(\d{2})", file
+    ):
+        year    = dates.group(1).translate(translation_table)
+        month   = dates.group(2).translate(translation_table)
+        day     = dates.group(3).translate(translation_table)
+        hour    = dates.group(4).translate(translation_table)
+        minute  = dates.group(5).translate(translation_table)
+        second  = dates.group(6).translate(translation_table)
+
         call(
-            f"touch -d '{dates.group(1)}/{dates.group(2)}/{dates.group(3)} {dates.group(4)}:{dates.group(5)}:{dates.group(6)}' '{file}'",
+            f"touch -d '{year}/{month}/{day} {hour}:{minute}:{second}' '{file}'",
             shell=True,
         )
