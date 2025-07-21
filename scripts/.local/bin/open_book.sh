cf9ce51ce418b8aeb44b0e0c556c47efd96ae069
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-07-21 11:07:44 +0330

    Make open_book.sh recognize epubs and djvu files besides pdf

--- scripts/.local/bin/open_book.sh
+++ scripts/.local/bin/open_book.sh
@@ -5,6 +5,6 @@ pdf_reader="zathura"
 launcher="rofi -dmenu -i -p book: "
 book_dir="${HOME}/Documents/books"
 cd "$book_dir"
-books="$(find . -iname '*.pdf' -type f -exec ls -1t "{}" +)"
+books="$(find . \( -name '*.pdf' -o -name '*.epub' -o -name '*.djvu' \) -type f -exec ls -1t "{}" +)"
 book="$(echo "$books" | $launcher)"
 "$pdf_reader" "$book" &
