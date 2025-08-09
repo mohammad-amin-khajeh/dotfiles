f866208eb97f2f46a6a3f4cb3d0cdd1971e9f630
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-08-09 14:46:47 +0330

    make wallpaper.sh posix-compliant

--- scripts/.local/bin/wallpaper.sh
+++ scripts/.local/bin/wallpaper.sh
@@ -1,16 +1,20 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/usr/bin/env sh
+
+# set the wallpaper and run pywal on it at the same time
+# uncomment the lines below to make pywal themeing work
+# otherwise just leave it as is to only set the wallpaper
 
 wallpath="${HOME}/Pictures/wallpapers"
+xresources_path="${XDG_CONFIG_HOME:-$HOME/.config}/X11/.Xresources"
+
 if [ $# -eq 0 ]; then
-  cd "$wallpath"
+  cd "$wallpath" || exit
   wall="$wallpath/$(fd -e png -e jpg -e jpeg | rofi -dmenu -i -p 'select wallpaper:')"
 else
   wall="$1"
 fi
 xwallpaper --zoom "$wall"
-echo -n "$wall" >"$XDG_CACHE_HOME"/wallpaper
-# wal -i "$wall"
+printf "%s" "$wall" >"$XDG_CACHE_HOME"/wallpaper
 
 Xresources_font_conf="Xft.antialias: 1
 Xft.autohint: 0
@@ -20,33 +24,28 @@ Xft.hintstyle: hintslight
 Xft.lcdfilter: lcddefault
 Xft.rgba: none"
 
-grep -q "$Xresources_font_conf" ~/.cache/wal/colors.Xresources || printf '\n%s' "$Xresources_font_conf" >>~/.cache/wal/colors.Xresources
+grep -q "$Xresources_font_conf" "$xresources_path" || printf '\n%s' "$Xresources_font_conf" >>"$xresources_path"
 
-# case "$XDG_SESSION_DESKTOP" in
+# wal -i "$wall"
+
+# dwm_normbgcolor="$(rg color0 -m1 "$xresources_path" | sed "s/^.*:/dwm.normbgcolor:/")"
+# dwm_normbordercolor="dwm.normbordercolor: \#ff0000"
+# dwm_normfgcolor="$(rg color2 -m1 "$xresources_path" | sed "s/^.*:/dwm.normfgcolor:/")"
+# dwm_selbgcolor="$(rg color5 -m1 "$xresources_path" | sed "s/^.*:/dwm.selbgcolor:/")"
+# dwm_selbordercolor="dwm.selbordercolor: \#00ff00"
+# dwm_selfgcolor="$(rg color9 -m1 "$xresources_path" | sed "s/^.*:/dwm.selfgcolor:/")"
+# dwm_selfloatcolor="$(rg color2 -m1 "$xresources_path" | sed "s/^.*:/dwm.selfloatcolor:/")"
+# dwm_normfloatcolor="$(rg color2 -m1 "$xresources_path" | sed "s/^.*:/dwm.normfloatcolor:/")"
+# dwm_tagsnormbgcolor="$(rg color0 -m1 "$xresources_path" | sed "s/^.*:/dwm.tagsnormbgcolor:/")"
+# dwm_tagsnormbordercolor="$(rg color3 -m1 "$xresources_path" | sed "s/^.*:/dwm.tagsnormbordercolor:/")"
+# dwm_tagsnormfgcolor="$(rg color2 -m1 "$xresources_path" | sed "s/^.*:/dwm.tagsnormfgcolor:/")"
+# dwm_tagsselbgcolor="$(rg color2 -m1 "$xresources_path" | sed "s/^.*:/dwm.tagsselbgcolor:/")"
+# dwm_tagsselbordercolor="$(rg color5 -m1 "$xresources_path" | sed "s/^.*:/dwm.tagsselbordercolor:/")"
+# dwm_tagsselfgcolor="$(rg color0 -m1 "$xresources_path" | sed "s/^.*:/dwm.tagsselfgcolor:/")"
+# dwm_titleselbgcolor="$(rg color2 -m1 "$xresources_path" | sed "s/^.*:/dwm.titleselbgcolor:/")"
+# dwm_titleselfgcolor="$(rg color0 -m1 "$xresources_path" | sed "s/^.*:/dwm.titleselfgcolor:/")"
 #
-# i3)
-# 	grep 'xargs --arg-file=.cache/wal/wal -d $ xwallpaper' ~/.config/i3/config >/dev/null ||
-# 		echo 'xargs --arg-file=.cache/wal/wal -d $ xwallpaper --zoom' >>~/.config/i3/config
-# 	;;
-# dwm)
-# 	dwm_normbgcolor="$(rg color0 -m1 ~/.Xresources | sed "s/^.*:/dwm.normbgcolor:/")"
-# 	dwm_normbordercolor="dwm.normbordercolor: \#ff0000"
-# 	dwm_normfgcolor="$(rg color2 -m1 ~/.Xresources | sed "s/^.*:/dwm.normfgcolor:/")"
-# 	dwm_selbgcolor="$(rg color5 -m1 ~/.Xresources | sed "s/^.*:/dwm.selbgcolor:/")"
-# 	dwm_selbordercolor="dwm.selbordercolor: \#00ff00"
-# 	dwm_selfgcolor="$(rg color9 -m1 ~/.Xresources | sed "s/^.*:/dwm.selfgcolor:/")"
-# 	dwm_selfloatcolor="$(rg color2 -m1 ~/.Xresources | sed "s/^.*:/dwm.selfloatcolor:/")"
-# 	dwm_normfloatcolor="$(rg color2 -m1 ~/.Xresources | sed "s/^.*:/dwm.normfloatcolor:/")"
-# 	dwm_tagsnormbgcolor="$(rg color0 -m1 ~/.Xresources | sed "s/^.*:/dwm.tagsnormbgcolor:/")"
-# 	dwm_tagsnormbordercolor="$(rg color3 -m1 ~/.Xresources | sed "s/^.*:/dwm.tagsnormbordercolor:/")"
-# 	dwm_tagsnormfgcolor="$(rg color2 -m1 ~/.Xresources | sed "s/^.*:/dwm.tagsnormfgcolor:/")"
-# 	dwm_tagsselbgcolor="$(rg color2 -m1 ~/.Xresources | sed "s/^.*:/dwm.tagsselbgcolor:/")"
-# 	dwm_tagsselbordercolor="$(rg color5 -m1 ~/.Xresources | sed "s/^.*:/dwm.tagsselbordercolor:/")"
-# 	dwm_tagsselfgcolor="$(rg color0 -m1 ~/.Xresources | sed "s/^.*:/dwm.tagsselfgcolor:/")"
-# 	dwm_titleselbgcolor="$(rg color2 -m1 ~/.Xresources | sed "s/^.*:/dwm.titleselbgcolor:/")"
-# 	dwm_titleselfgcolor="$(rg color0 -m1 ~/.Xresources | sed "s/^.*:/dwm.titleselfgcolor:/")"
-#
-# 	echo "! dwm stuff
+# echo "! dwm stuff
 # $dwm_normbgcolor
 # $dwm_normbordercolor
 # $dwm_normfgcolor
@@ -62,12 +61,6 @@ grep -q "$Xresources_font_conf" ~/.cache/wal/colors.Xresources || printf '\n%s' 
 # $dwm_tagsselbordercolor
 # $dwm_tagsselfgcolor
 # $dwm_titleselbgcolor
-# $dwm_titleselfgcolor" >>~/.Xresources
-#
-# 	xrdb -load ~/.Xresources && xdotool key Super+Shift+F5
-# 	;;
-# *)
-# 	echo "the wm is not supported, aborting..."
-# 	return 1
-# 	;;
-# esac
+# $dwm_titleselfgcolor" >>"$xresources_path"
+
+# xrdb -load "$xresources_path" && xdotool key Super+Shift+F5
