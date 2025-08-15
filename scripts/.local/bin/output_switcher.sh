b4c603b5db3eac0796e50a791937a57052d61296
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-08-16 01:51:45 +0330

    Update output_switcher script

--- scripts/.local/bin/output_switcher.sh
+++ scripts/.local/bin/output_switcher.sh
@@ -1,14 +1,16 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 mode=$(printf "tv\ninternal\n" | rofi -dmenu -p "select the mode please:")
 case "$mode" in
 tv)
+  command -v redshift >/dev/null && redshift -x
   xrandr --output HDMI-1 --mode 1920x1080 --output eDP-1 --off ||
     xrandr --addmode HDMI-1 1920x1080 &&
     xrandr --output HDMI-1 --mode 1920x1080 --output eDP-1 --off
   pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo+input:analog-stereo
   ;;
 internal)
+  command -v redshift >/dev/null && redshift -x && redshift -O 2000k
   xrandr --output eDP-1 --mode 1920x1080 --output HDMI-1 --off
   pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
   ;;
