14f69b62898eebc74092b416b8801fd699c11665
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-08-25 00:23:03 +0330

    handle edge cases in leetcode_session.sh

--- tmux/.config/tmux/leetcode_session.sh
+++ tmux/.config/tmux/leetcode_session.sh
@@ -2,8 +2,12 @@
 
 sesh_name="code"
 
-if tmux has-session -t "$sesh_name">/dev/null; then
-  tmux attach-session -t "$sesh_name"
+if tmux has-session -t "$sesh_name" 2>/dev/null; then
+  if [ -z "$TMUX" ]; then
+    exec tmux attach-session -t "$sesh_name"
+  else
+    echo "you're already in tmux."
+  fi
   exit 0
 fi
 
@@ -11,15 +15,17 @@ fi
 tmux new-session -d -s "$sesh_name" -n leetcode -c ~/Documents/code/python/leetcode/
 
 # Create second window named 'cmus' and open cmus in it
-tmux new-window -t "$sesh_name": -n cmus "cmus"
+tmux new-window -t "$sesh_name" -n cmus "cmus"
 
 # Create third window named 'timer'
-tmux new-window -t "$sesh_name": -n timer "sh"
+tmux new-window -t "$sesh_name" -n timer "sh"
 
 # Type the 'study 30m' command in the first pane of the timer window without pressing enter automatically (simulate sleep then send keys without Enter)
 sleep 0.2
 tmux send-keys -t "$sesh_name":timer "study 30m"
 
-# Finally, attach to the session and focus on the first window
+# Focus on the first window
 tmux select-window -t "$sesh_name":leetcode
-tmux attach-session -t "$sesh_name"
+
+# Finally, attach or switch
+[ -z "$TMUX" ] && tmux attach-session -t "$sesh_name" || tmux switch-client -t "$sesh_name":leetcode
