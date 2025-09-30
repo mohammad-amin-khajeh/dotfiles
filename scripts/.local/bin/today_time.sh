885d8e68541171bd0a0a3797fff0dd44abcdfbb1
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-09-30 14:56:36 +0330

    Switch to markdown from norg for progress tracking

--- scripts/.local/bin/today_time.sh
+++ scripts/.local/bin/today_time.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 # gives the total studying, work time of the last logged day
 # the following format is mandatory:
 # *empty line*
-# %664 days left%
-# *** 13 Sun => 3:35 %6 pages read - at 18%
-#     ~ anki: 2:08
-#     ~ leetcode: 0:07
-#     ~ study proof writing - how to prove it: 1:20
+# ### 13 Sun => 3:35 <!-- 6 pages read - at 18 -->
+# - anki: 2:08
+# - leetcode: 0:07
+# - study proof writing - how to prove it: 1:20
 #
-# only the lines beginning with ~ are considered.
+# only the lines beginning with - are considered.
 
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 filename"
@@ -19,11 +18,11 @@ fi
 filename=$1
 max_tasks_in_a_day=30
 
-tail -n "$max_tasks_in_a_day" "$filename" |
-  tac |
-  awk '
+tail -n "$max_tasks_in_a_day" "$filename" \
+  | tac \
+  | awk '
   !NF { exit }
-  $1=="~" {
+  $1=="-" {
     if (match($0, /([0-9]+):([0-9]{2})[[:space:]]*$/, m)) {
       h = m[1] + 0
       mns = m[2] + 0
