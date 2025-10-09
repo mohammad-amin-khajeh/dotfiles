5422c2bad57701796498b12cd1fb5b2619e66d2f
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-10-09 14:12:35 +0330

    Update calcurse.sh

--- scripts/.local/bin/calcurse.sh
+++ scripts/.local/bin/calcurse.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 todo_path="$XDG_DATA_HOME/calcurse/todo"
-todo_text="$(rofi -dmenu -p "what is the TODO?")"
-todo_prio="$(rofi -dmenu -p "what is the priority? (leave empty for 0)")"
+todo_text="$(rofi -dmenu -p "what is the TODO? ")"
+[ "$todo_text" != "" ] && todo_prio="$(rofi -dmenu -p "what is the priority? (leave empty for 0)")" || exit 1
 todo_item="[${todo_prio:-0}] $todo_text"
 
 echo "temp" >> "$todo_path"
