365420f82c0ecf7dd02f9f59274bdf7838110d0b
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2026-02-01 16:09:58 +0330

    Use a new starter tmux session

--- scripts/.local/bin/launch_tmux.sh
+++ scripts/.local/bin/launch_tmux.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
-if pgrep tmux:\ server >/dev/null 2>&1; then
+session_name="study_session.sh"
+
+if pgrep tmux:\ server > /dev/null 2>&1; then
   tmux attach-session
 else
-  exec "$XDG_CONFIG_HOME"/tmux/leetcode_session.sh
+  exec "$XDG_CONFIG_HOME"/tmux/"$session_name"
 fi
---
+++ tmux/.config/tmux/study_session.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+sesh_name="study"
+
+if tmux has-session -t "$sesh_name" 2> /dev/null; then
+  if [ -z "$TMUX" ]; then
+    exec tmux attach-session -t "$sesh_name"
+  else
+    echo "you're already in tmux."
+  fi
+  exit 0
+fi
+
+tmux new-session -d -s "$sesh_name" -n timer "sh"
+
+tmux send-keys -t "$sesh_name":timer "study"
+
+tmux select-window -t "$sesh_name":timer
+
+# Attach or switch
+if [ -z "$TMUX" ]; then tmux attach-session -t "$sesh_name"; else
+  tmux switch-client -t "$sesh_name":timer
+fi
